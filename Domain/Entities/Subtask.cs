30db793b5e172b88c15ea063152c12a829699d11
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-10 23:04:27 -0300

    Criada a entidade Subtask

---
+++ Domain/Entities/Subtask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities {
+    public class Subtask : BaseEntity {
+        public string? Description { get; set; }
+        public bool IsDone { get; set; } = false;
+
+        public int TaskItemId { get; set; }
+        public TaskItem? TaskItem { get; set; }
+    }
+}
