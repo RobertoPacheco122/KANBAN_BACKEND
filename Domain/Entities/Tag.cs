724c8809bdde0ae98ae9a56521943fee9121bf0f
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-10 23:04:45 -0300

    Criada a entidade Tag

---
+++ Domain/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities {
+    public class Tag : BaseEntity {
+        public string Name { get; set; } = string.Empty;
+        public string BackgroundColorInHexa { get; set; } = string.Empty;
+        public string TextColorInHexa { get;set; } = string.Empty;
+
+        public List<TaskItem>? TaskItems { get; set; }
+    }
+}
