90245bc549ec73144f9151fdd65170cf778c4fd0
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-10 23:05:11 -0300

    Criada a entidade ApplicationUser

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities {
+    public class ApplicationUser : BaseEntity {
+        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+    }
+}
