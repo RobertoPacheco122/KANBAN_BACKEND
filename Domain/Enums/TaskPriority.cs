b330b618a2dd9d22630f98000cd189e00be6e928
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-10 23:05:26 -0300

    Criada a enum TaskPriority

---
+++ Domain/Enums/TaskPriority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums {
+    public enum TaskPriority {
+        LowPriority = 1,
+        MidPriority = 2,
+        HighPriority = 3
+    }
+}
