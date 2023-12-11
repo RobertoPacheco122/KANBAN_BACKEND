6dd207292da6233748b3f2a5af7cb58a46366774
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-10 23:04:12 -0300

    Criada a entidade List

---
+++ Domain/Entities/List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities {
+    public class List : BaseEntity {
+        public string Name { get; set; } = string.Empty;
+
+        public int BoardId { get; set; }
+        public Board? Board { get; set; }
+        public List<TaskItem>? TaskItems { get; set; }
+    }
+}
