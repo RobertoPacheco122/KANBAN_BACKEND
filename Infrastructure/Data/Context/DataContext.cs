5d85eaa1d4491359698f14749c11413937a91233
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-18 17:38:17 -0300

    Criado o DataContext com os DbSets das entidades do Domain

---
+++ Infrastructure/Data/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Context {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ApplicationUserEntity> Users => Set<ApplicationUserEntity>();
+        public DbSet<BoardEntity> Boards => Set<BoardEntity>();
+        public DbSet<ListEntity> Lists => Set<ListEntity>();
+        public DbSet<SubtaskEntity> Subtasks => Set<SubtaskEntity>();
+        public DbSet<TagEntity> Tags => Set<TagEntity>();
+        public DbSet<TaskEntity> Tasks => Set<TaskEntity>();
+
+    }
+}
