91b7596664f4831123b5feeeebc7fd4af8cb0535
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-18 17:39:00 -0300

    Criado arquivo com as configurações de acesso ao banco de dados

---
+++ Infrastructure/Data/Context/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Context {
+    public class ContextFactory : IDesignTimeDbContextFactory<DataContext> {
+        public DataContext CreateDbContext(string[] args) {
+            var connectionString = "Host=localhost;Port=5432;Database=kanbandddapi;Username=postgres;Password=fanta123";
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
