d1c0dbb30515a7026302d4d638552a080f19a0e3
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-18 17:37:08 -0300

    Criada interface IRepository

---
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories {
+    public interface IRepository<T> where T : BaseEntity {
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> InsertAsync(T entity);
+        Task<T> SelectAsync(int id);
+        Task<T> UpdateAsync(T entity);
+    }
+}
