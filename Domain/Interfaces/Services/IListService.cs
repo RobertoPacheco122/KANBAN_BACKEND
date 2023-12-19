8ef5b371b86f3f2eb59a946be014947290e6c6d4
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-19 14:04:13 -0300

    Criada a interface de IListService

---
+++ Domain/Interfaces/Services/IListService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services {
+    public interface IListService {
+        Task<bool> Delete(int id);
+        Task<List<ListEntity>> GetAll();
+        Task<ListEntity> GetSingle(int id);
+        Task<ListEntity> Insert(ListEntity list);
+        Task<ListEntity> Update(ListEntity list);
+    }
+}
