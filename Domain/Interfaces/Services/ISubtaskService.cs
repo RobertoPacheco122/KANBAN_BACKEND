0331722b903cba592e24132f2b0ef162374da254
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-19 14:04:34 -0300

    Criada a interface de ISubtaskService

---
+++ Domain/Interfaces/Services/ISubtaskService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services {
+    public interface ISubtaskService {
+        Task<bool> Delete(int id);
+        Task<List<SubtaskEntity>> GetAll();
+        Task<SubtaskEntity> GetSingle(int id);
+        Task<SubtaskEntity> Insert(SubtaskEntity subtask);
+        Task<SubtaskEntity> Update(SubtaskEntity subtask);
+    }
+}
