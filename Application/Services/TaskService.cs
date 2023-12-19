6475b937d18a9a217a829caec81b7551bfc3191f
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-19 14:10:09 -0300

    Implementado o service de Task

---
+++ Application/Services/TaskService.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services {
+    public class TaskService : ITaskService {
+        private readonly IRepository<TaskEntity> _taskRepository;
+        public TaskService(IRepository<TaskEntity> taskRepository) {
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<bool> Delete(int id) {
+            return await _taskRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<TaskEntity>> GetAll() {
+            return await _taskRepository.GetAllAsync();
+        }
+
+        public async Task<TaskEntity> GetSingle(int id) {
+            return await _taskRepository.SelectAsync(id);
+        }
+
+        public async Task<TaskEntity> Insert(TaskEntity task) {
+            return await _taskRepository.InsertAsync(task);
+        }
+
+        public async Task<TaskEntity> Update(TaskEntity task) {
+            return await _taskRepository.UpdateAsync(task);
+        }
+    }
+}
