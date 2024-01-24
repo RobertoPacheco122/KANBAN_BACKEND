513d52e160a0dd1778febe9e9d0d6af7a294a136
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-24 19:11:07 -0300

    Revert "Adicionado referência para Domain em Application"
    This reverts commit d50857bcda2bad5ef7297e57d19ef8c950c8ed48.

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,6 @@
-using Infrastructure.CrossCutting.DependencyInjection;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
