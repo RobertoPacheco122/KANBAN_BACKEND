0cb6e087e7378a3ce8f435f754e5b4215eaa8bbc
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-25 19:18:24 -0300

    Alterado os parâmetros que utilizar o id em int para Guid em IRepository

--- Domain/Interfaces/Repositories/IRepository.cs
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Domain.Interfaces.Repositories {
     public interface IRepository<T> where T : BaseEntity {
-        Task<bool> DeleteAsync(int id);
-        Task<bool> ExistAsync(int id);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ExistAsync(Guid id);
         Task<List<T>> GetAllAsync();
         Task<T> InsertAsync(T entity);
-        Task<T> SelectAsync(int id);
+        Task<T> SelectAsync(Guid id);
         Task<T> UpdateAsync(T entity);
     }
 }
