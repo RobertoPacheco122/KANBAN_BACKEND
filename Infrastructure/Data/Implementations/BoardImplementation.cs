17fa86e986a985336af55cdc942702c94ccb7282
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-25 22:09:14 -0300

    Criada a implementation de Board

---
+++ Infrastructure/Data/Implementations/BoardImplementation.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Data.Context;
+using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Implementations {
+    public class BoardImplementation : BaseRepository<BoardEntity>, IBoardRepository {
+        private DbSet<BoardEntity> _dataset;
+
+        public BoardImplementation(DataContext context) : base(context) {
+            _dataset = context.Set<BoardEntity>();
+        }
+
+        public async Task<BoardEntity> GetAllRelatedDetails(Guid id) {
+            try {
+                var queryResult = await _dataset.
+                    Include(board => board.Lists)
+                        .ThenInclude(list => list.Tasks)
+                            .ThenInclude(task => task.Users)
+                                .ThenInclude(user => user.Tasks)
+                            .ThenInclude(task => task.Tags)
+                    .FirstOrDefaultAsync(board => board.Id == id);
+
+                return queryResult;
+            } catch (Exception expection) {
+                throw;
+            }
+        }
+    }
+}
