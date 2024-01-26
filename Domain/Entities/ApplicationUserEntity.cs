18359e21932a1801bd8eea47289cdabc612883d1
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-25 22:11:50 -0300

    Configurada a dependency injection de IBoardRepository e BoardImplementation, adicionado o método no Controller e utilizando o JsonIgnore nas entities

--- Domain/Entities/ApplicationUserEntity.cs
+++ Domain/Entities/ApplicationUserEntity.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities {
@@ -13,6 +14,7 @@ namespace Domain.Entities {
         public string Password { get; set; } = string.Empty;
         public EUserRole Role { get; set; }
 
+        [JsonIgnore]
         public List<TaskEntity>? Tasks { get; set; }
     }
 }
