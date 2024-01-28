5cce6614c51874e23a23ffbe144e97b3e2819a88
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-28 20:24:54 -0300

    Realocado os arquivos ApplicationModule e InstrastructureModule para a nova solution CrossCutting

---
+++ CrossCutting/DependencyInjection/InfrastructureModule.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces.Repositories;
+using Infrastructure.Data.Context;
+using Infrastructure.Data.Repositories;
+using Infrastructure.Data.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CrossCutting.DependencyInjection {
+    public static class InfrastructureModule {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+            services.AddRepositories();
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services) {
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IBoardRepository, BoardImplementation>();
+
+            services.AddDbContext<DataContext>(
+                options => options.UseNpgsql("Host=localhost;Port=5432;Database=kanbandddapi;Username=postgres;Password=fanta123")
+            );
+
+            return services;
+        }
+    }
+}
