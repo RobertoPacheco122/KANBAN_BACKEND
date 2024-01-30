48304e4c59d43e2aa527e6966b5e6d3ee7e8939c
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-29 21:41:12 -0300

    Criado o arquivo ListDto

---
+++ Domain/DTOs/List/ListDto.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.List {
+    public class ListDto : BaseEntity {
+        public string Name { get; set; } = string.Empty;
+        public Guid BoardId { get; set; }
+    }
+}
