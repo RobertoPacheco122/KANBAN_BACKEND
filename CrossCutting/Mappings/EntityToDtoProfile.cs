97f68d7a99d64e627b465e19c3a284a7e1f40830
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-29 21:45:44 -0300

    Criado o arquivo profile de conversão das Lists

---
+++ CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Domain.DTOs.List;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.Mappings {
+    public class EntityToDtoProfile : Profile {
+        public EntityToDtoProfile() {
+            CreateMap<ListAddDto, ListEntity>()
+                .ReverseMap();
+
+            CreateMap<ListUpdateDto, ListEntity>()
+                .ReverseMap();
+
+            CreateMap<ListDto, ListEntity>()
+                .ReverseMap();
+        }
+    }
+}
