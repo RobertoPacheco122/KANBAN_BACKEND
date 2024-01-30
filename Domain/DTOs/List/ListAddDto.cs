e3900486fb16a1b97e33303f1319a81e72c648fe
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-01-29 21:41:23 -0300

    Criado o arquivo ListAddDto

---
+++ Domain/DTOs/List/ListAddDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.List {
+    public class ListAddDto {
+        [Required(ErrorMessage = "É necessário fornecer o Id da List para realizar o inserir ou atualizar a List")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "É necessário fornecer o Id do Board para realizar o inserir ou atualizar a List")]
+        public Guid BoardId { get; set; }
+        
+    }
+}
